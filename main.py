5519575ded057cbd44f88ed17abbcdeda9fd75e5
park chan <parkchan428@gmail.com>
2018-01-26 11:18:06 +0900

    id값으로 앨범 정보 가져오기

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from utils import get_top100_list, get_song_detail
 if __name__ == '__main__':
     result = get_top100_list()
     for item in result:
-        # print(f'{item["song_tag"]}, {item["artist"]}, {item["title"]}')
+        print(f'{item["rank"]:3}: {item["song_id"]}|{item["title"]}')
 
-
+    # 예제
+    result_detail = get_song_detail('30781481')
